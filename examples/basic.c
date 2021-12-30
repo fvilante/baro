d7d41acc6d1942beaaea6622c0dae1bffaa13d9c
Brandon Walker <brandon@branw.xyz>
2021-12-29 21:02:10 -0500

    Update basic example

--- examples/basic.c
+++ examples/basic.c
@@ -1,6 +1,45 @@
 #include <baro.h>
 
 TEST("basic") {
+    CHECK(1);
+    CHECK(0);
+
+    CHECK_FALSE(0);
+    CHECK_FALSE(1);
+
+    CHECK_EQ(0, 0);
+    CHECK_EQ(0, 1);
+    CHECK_EQ(1, 0);
+    CHECK_EQ(1, 1);
+
+    CHECK_NE(0, 0);
+    CHECK_NE(0, 1);
+    CHECK_NE(1, 0);
+    CHECK_NE(1, 1);
+
+    CHECK_LT(0, 0);
+    CHECK_LT(0, 1);
+    CHECK_LT(1, 0);
+    CHECK_LT(1, 1);
+
+    CHECK_LE(0, 0);
+    CHECK_LE(0, 1);
+    CHECK_LE(1, 0);
+    CHECK_LE(1, 1);
+
+    CHECK_GE(0, 0);
+    CHECK_GE(0, 1);
+    CHECK_GE(1, 0);
+    CHECK_GE(1, 1);
+
+    CHECK_GT(0, 0);
+    CHECK_GT(0, 1);
+    CHECK_GT(1, 0);
+    CHECK_GT(1, 1);
+}
+
+TEST("require") {
     REQUIRE(1);
-    REQUIRE_FALSE(0);
+
+    REQUIRE(0);
 }
